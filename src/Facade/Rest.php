a4d33d4f11d9baa751b6d9e9e6bdcb594b0456f6
vahiidrah <vahiid_rah@yahoo.com>
2022-02-19 23:46:25 +0330

    add Rest facade ide helper

--- src/Facade/Rest.php
+++ src/Facade/Rest.php
@@ -4,19 +4,18 @@ namespace Alirah\LaravelRest\Facade;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static ok(mixed $array)
+ * @method static accepted(mixed $array)
+ * @method static badRequest(mixed $array)
+ * @method static unauthorized(mixed $array)
+ * @method static forbidden(mixed $array)
+ * @method static notFound(mixed $array)
+ * @method static error(mixed $array)
+ * @method static custom(mixed $array , int $statusCode)
+ */
 class Rest extends Facade
 {
-    /**
-     * @method static ok(mixed $array)
-     * @method static accepted(mixed $array)
-     * @method static badRequest(mixed $array)
-     * @method static unauthorized(mixed $array)
-     * @method static forbidden(mixed $array)
-     * @method static notFound(mixed $array)
-     * @method static error(mixed $array)
-     * @method static custom(mixed $array , int $statusCode)
-     */
-
     /**
      * Get the binding in the IOC container
      *
